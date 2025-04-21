6542c6999191dfdcf96ae9e1db602fc3bbc349dd
Peng Wang <peng.wang@btrust.intl>
2025-04-21 16:51:31 -0400

    update config

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql+asyncpg://postgres:Btrust123@172.16.10.138:5432/invoice"
+DATABASE_URL = "postgresql+asyncpg://postgres:Btrust123@localhost:5432/invoice"
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 AsyncSessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
