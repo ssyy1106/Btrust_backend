ca0219882119a41b5670540ef724756cf1c8aecc
Peng Wang <peng.wang@btrust.intl>
2025-08-11 11:24:34 -0400

    update init db

--- init_db.py
+++ init_db.py
@@ -1,14 +1,18 @@
 import asyncio
 from sqlalchemy.ext.asyncio import create_async_engine
 from models.stock import Base_stock
+from models.storestock import Base_storestock
 from models.invoice import Base_invoice
 from models.cost import Base_cost
-from database import DATABASE_URL_STOCK, DATABASE_URL_INVOICE, DATABASE_URL_COST
+from database import DATABASE_URL_STOCK, DATABASE_URL_INVOICE, DATABASE_URL_COST, DATABASE_URL_STORESTOCK
 
 async def init_db():
-    engine = create_async_engine(DATABASE_URL_STOCK, echo=True)
-    async with engine.begin() as conn:
+    engine_stock = create_async_engine(DATABASE_URL_STOCK, echo=True)
+    async with engine_stock.begin() as conn:
         await conn.run_sync(Base_stock.metadata.create_all)
+    engine_storestock = create_async_engine(DATABASE_URL_STORESTOCK, echo=True)
+    async with engine_storestock.begin() as conn:
+        await conn.run_sync(Base_storestock.metadata.create_all)
     # engine2 = create_async_engine(DATABASE_URL_INVOICE, echo=True)
     # async with engine2.begin() as conn:
     #     await conn.run_sync(Base_invoice.metadata.create_all)
